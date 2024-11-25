acb3b0d1049b0268bea9f8c144889779545d7c39
Saoud ElTelawy <30501682+saoudeltelawy@users.noreply.github.com>
2024-11-25 18:04:11 +0200

    Update rock_paper_scissors_V2.py
    Inject the ASCII art into a list!

--- rock_paper_scissors_V2.py
+++ rock_paper_scissors_V2.py
@@ -25,31 +25,34 @@ scissors = '''
 ---.__(___)
 '''
 
-
+# -------------
 # Reference:
+# -------------
 # 0: Rock
 # 1: Paper
 # 2: Scissors
-
+# -------------
 
 import random
 computer_choice = random.randint(0,2)
 
 user_choice = int(input('Please select 0 for "Rock" and 1 for "Paper" and 2 for "Scissors" \n'))
 
-# print(user_choice)
-print(computer_choice)
+game_images = [rock, paper, scissors]
+
+print(game_images[user_choice])
+print(game_images[computer_choice])
+
 
 if user_choice == 0:
-  print(rock)
   if computer_choice == 0:
-    print(rock)
+    # print(rock)
     print(f"You choose Rock and the computer Choose rock so it is a tie!")
   elif computer_choice == 1:
-    print(paper)
+    # print(paper)
     print(f"You choose Rock and the computer Choose paper so you Lose!!!")
   else:
-    print(scissors)
+    # print(scissors)
     print(f"You choose Rock and the computer Choose Scissors so you win :D")
 elif user_choice == 1:
   print(paper)
@@ -60,7 +63,7 @@ elif user_choice == 1:
   else:
     print(f"You choose Paper and the computer Choose Scissors so you lose :P")
 elif user_choice == 2:
-  print(scissors)
+  # print(scissors)
   if computer_choice == 0:
     print(f"You choose Scissors and the computer Choose rock so you lose!")
   elif computer_choice == 1:
